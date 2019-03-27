cec5425ef2e6ccd870d1b09b80718359f8ce454c
yangjiqiang <1005473728@qq.com>
2019-03-27 17:26:49 +0800

    update: 表格显示顺序调整

--- src/js/common/template.js
+++ src/js/common/template.js
@@ -1,22 +1,22 @@
 export let th=`<tr>
-          <th>股票名称</th>
           <th>股票代码</th>
-          <th>更新时间</th>
+          <th>股票名称</th>
           <th>单价</th>
           <th>涨幅</th>
           <th>成交量</th>
+          <th>更新时间</th>
           <th>公式</th>
         </tr>`;
 
 export let td=(val)=>{
 
   return        `<tr>
-                    <td>${val.stock_name}</td>
                     <td>${val.stock_code}</td>
-                    <td>${val.update_time}</td>
+                    <td>${val.stock_name}</td>
                     <td>${val.formula_time}</td>
                     <td class="${(val.num1.indexOf('-')!==(-1))?'green':'red'}">${val.num1}</td>
                     <td>${val.num2}</td>
+                    <td>${val.update_time}</td>
                     <td>${val.num3}</td>
                 </tr>`
 }
